7e3142f221a5d43e9402641ba3ee917b780094fc
fnriv <fnrivera@ucsd.edu>
2025-04-06 23:27:22 -0700

    basic html

--- nextjs-with-sass/components/gallery/gallery.jsx
+++ nextjs-with-sass/components/gallery/gallery.jsx
@@ -7,6 +7,27 @@ export default function Gallery() {
     return (
         <div className="gallery__container">
             <h1>Hello gallery</h1>
+            <img
+                src="/img3.png"
+                alt="Image"
+                width={500}
+                height={500}
+                className="title__image"
+            />
+            <img
+                src="/img4.png"
+                alt="Image"
+                width={500}
+                height={500}
+                className="title__image"
+            />
+            <img
+                src="/img5.png"
+                alt="Image"
+                width={500}
+                height={500}
+                className="title__image"
+            />
         </div>
     )
 }
--- nextjs-with-sass/components/title/title.jsx
+++ nextjs-with-sass/components/title/title.jsx
@@ -2,11 +2,20 @@
 
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Title() {
     return (
         <div className="title__container">
             <h1>Hello title</h1>
+            <img
+                src="/img2.png"
+                alt="Image"
+                width={500}
+                height={500}
+                className="title__image"
+            />
+            
         </div>
     )
 }
