3b46cca34c9e1a0a48e8e0ee8be0b8e6a54f3cf1
fnriv <fnrivera@ucsd.edu>
2025-04-07 23:37:32 -0700

    regular screen css finished

--- nextjs-with-sass/components/gallery/gallery.jsx
+++ nextjs-with-sass/components/gallery/gallery.jsx
@@ -7,27 +7,24 @@ import styles from './gallery.module.scss';
 export default function Gallery() {
     return (
         <div className={styles.gallery__container}>
-            <img
-                src="/img3.png"
-                alt="Park 2"
-                width={296}
-                height={447}
-                className={styles.img}
-            />
-            <img
-                src="/img4.png"
-                alt="Park 3"
-                width={296}
-                height={447}
-                className={styles.img}
-            />
-            <img
-                src="/img5.png"
-                alt="Park 4"
-                width={296}
-                height={447}
-                className={styles.img}
-            />
+            <div className={styles.diagonal__container}>
+                <img
+                    src="/img3.png"
+                    alt="Park 2"
+                    className={styles.img}
+                />
+                <img
+                    src="/img4.png"
+                    alt="Park 3"
+                    className={styles.img}
+                />
+                <img
+                    src="/img5.png"
+                    alt="Park 4"
+                    className={styles.img}
+                />
+            </div>
+            
         </div>
     )
 }
